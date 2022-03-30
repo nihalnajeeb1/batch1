2b97d24eb0f0ba48a60dbff8403bce58dd137642
nihalnajeeb1 <nihalncr7@gmail.com>
2022-03-30 11:28:18 +0530

    Update

--- education/urls.py
+++ education/urls.py
@@ -1,3 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+app_name = 'education'
+from education import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.learn, name='index'),
+    path('about/', views.about, name='about'),
+    path('courses/', views.courses, name='courses')
 ]
--- education/views.py
+++ education/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def learn(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def courses(request):
+    return render(request, 'courses.html')
